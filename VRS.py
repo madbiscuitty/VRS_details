207fae0fc7245493b8094b1734504dda959e29b9
madbiscuitty <madbiscuitty@gmail.com>
2022-08-27 19:14:59 +0300

    v 0.0.5

--- VRS.py
+++ VRS.py
@@ -4,9 +4,9 @@ from tkinter import messagebox
 from tkinter.ttk import Combobox
 from table_var import *
 
-wb_form = load_workbook(filename=r'resourses/vrs_details.xlsx', read_only=True)
 wb = Workbook()
 ws = wb.active
+#wb.worksheets[sht].clear()
 ws.title = 'Project'
 
 class VRS:
@@ -16,16 +16,20 @@ class VRS:
         self.root.resizable(resizable[0], resizable[1])
         if icon:
             self.root.iconbitmap(icon)
-
+        #объекты виджетов окна
         self.vrs_num = Combobox(self.root, values=("019", "034", "039", "054", "058", "078", "086", "097", "115",
                                                    "116", "138", "156", "173", "193", "194", "215", "234", "240",
                                                    "271", "289", "290", "333", "337", "350", "407", "414", "473",
                                                    "500"), width=7, state="readonly")
         self.vrs_perf = Combobox(self.root, values=("01", "02", "03", "04"), width=7, state="readonly")
-        self.air = Combobox(self.root, values=("АИР56", "АИР64", "АИР71", "АИР80", "АИР90", "АИР100", "АИР112",
+        self.air = Combobox(self.root, values=("АИР56", "АИР63", "АИР71", "АИР80", "АИР90", "АИР100", "АИР112",
                                                "АИР132", "АИР160", "АИР180", "АИР225", "АИР250"), width=7, state="readonly")
-        self.air2 = Combobox(self.root, values=("АИР56", "АИР64", "АИР71", "АИР80", "АИР90", "АИР100", "АИР112",
+        self.air2 = Combobox(self.root, values=("АИР56", "АИР63", "АИР71", "АИР80", "АИР90", "АИР100", "АИР112",
                                                 "АИР132", "АИР160", "АИР180", "АИР225", "АИР250"), width=7, state="readonly")
+        self.vosk = Combobox(self.root, values=("2.5", "2.8", "3.15", "3.55", "4.0", "4.5", "5.0", "5.6", "6.3", "7.1",
+                                                "8.0", "9.0", "10.0", "11.2", "12.5"), width=7, state="readonly")
+        self.vosk2 = Combobox(self.root, values=("2.5", "2.8", "3.15", "3.55", "4.0", "4.5", "5.0", "5.6", "6.3", "7.1",
+                                                "8.0", "9.0", "10.0", "11.2", "12.5"), width=7, state="readonly")
         self.vnv5012_widht = Combobox(self.root, values=("160", "180"), width=7, state="readonly")
        # self.vnv4816_widht = Combobox(self.root, values=("240", "320", "400"), width=7, state="readonly")
         self.vov5012_widht = Combobox(self.root, values=("180", "220", "260", "310"), width=7, state="readonly")
@@ -34,9 +38,9 @@ class VRS:
         self.ventbox = IntVar()
         self.vent2box = IntVar()
         self.vnv5012box = IntVar()
-        self.vnv4816box = IntVar()
+        #self.vnv4816box = IntVar()
         self.vov5012box = IntVar()
-        self.vov4816box = IntVar()
+        #self.vov4816box = IntVar()
         self.ekobox = IntVar()
         self.vertklapbox = IntVar()
         self.ppbox = IntVar()
@@ -54,77 +58,106 @@ class VRS:
         self.entrypp = Spinbox(self.root, values=(count), bd=2)
         self.entrypromkam = Spinbox(self.root, values=(count), bd=2)
 
-
+    #Запуск окна программы
     def run(self):
         self.draw_widgets()
         self.root.mainloop()
-
+    #Построение окна программы
     def draw_widgets(self):
-        Label(self.root, text="Выберите типоразмер VRS:", justify=LEFT).grid(row=0, column=0, sticky=W)                                   # вывод строки с текстом
-        self.vrs_num.grid(row=0, column=1, sticky=W + E, padx=5, pady=5)                                                            # вывод выпадающего меню
+        Label(self.root, text="Выберите типоразмер VRS:", justify=LEFT, font=("", 9, "bold")).grid(row=0, column=0,
+                                                                                                    sticky=W)
+        self.vrs_num.grid(row=0, column=1, columnspan=3, sticky=W + E, padx=5, pady=5)
         self.vrs_num.current(0)                                                                                                     # устанавливаем дефолтную позицию выпадающего меню
-        Label(self.root, text="Выберите исполнение VRS:", justify=LEFT).grid(row=1, column=0, sticky=W)                            # вывод строки с текстом
-        self.vrs_perf.grid(row=1, column=1, sticky=W + E, padx=5, pady=5)                                                          # вывод выпадающего меню
+        Label(self.root, text="Выберите исполнение VRS:", justify=LEFT, font=("", 10, "bold")).grid(row=1, column=0, sticky=W)
+        self.vrs_perf.grid(row=1, column=1, columnspan=3, sticky=W + E, padx=5, pady=5)       # вывод выпадающего меню
         self.vrs_perf.current(0)                                                                                                    # устанавливаем дефолтную позицию выпадающего меню
-        Label(self.root, text="Выберите блоки и количество:", justify=LEFT).grid(row=2, column=0, sticky=W)                         # вывод строки с текстом
-        Checkbutton(self.root, text="Блок фильтра", justify=LEFT, variable=self.filterbox).grid(row=3, column=0, sticky=W)          # строка чекбокса
-        self.entryfilter.grid(row=3, column=1, columnspan=2, sticky=W)  # рисуем спинбокс
-        Checkbutton(self.root, text="Блок вентилятора ВОСК", justify=LEFT, variable=self.ventbox).grid(row=4,
+        Label(self.root, text="Выберите параметры блоков:", justify=LEFT, font=("", 12, "bold")).grid(
+            row=2, column=0, sticky=W) # вывод строки с текстом
+        Button(self.root, width=2, height=1, text="?", command=self.info).grid(row=2, column=1, sticky=W)
+        Checkbutton(self.root, text="Блок фильтра", justify=LEFT, font=("", 9, "bold"), variable=self.filterbox).grid(
+            row=3, column=0,
+                                                                                                 sticky=W)          # строка чекбокса
+        self.entryfilter.grid(row=3, column=1, columnspan=3, sticky=W+E, padx=5, pady=5)  # рисуем спинбокс
+        Checkbutton(self.root, text="Блок вентилятора ВОСК", font=("", 9, "bold"), justify=LEFT,
+                    variable=self.ventbox).grid(row=4,
                                                                                                        column=0, sticky=W)   # строка чекбокса
-        self.air.grid(row=4, column=1, sticky=W)           # выпадающая менюшка аиров
+        self.vosk.grid(row=4, column=1, sticky=W, padx=5, pady=5)
+        self.vosk.current(0)
+        self.air.grid(row=4, column=2, sticky=W, padx=5, pady=5)           # выпадающая менюшка аиров
         self.air.current(0)  # устанавливаем дефолтную позицию выпадающего меню
-        self.entryvent.grid(row=4, column=2, sticky=W)
-        Checkbutton(self.root, text="Блок вентилятора ВОСК 2", justify=LEFT, variable=self.vent2box).grid(row=5,
+        self.entryvent.grid(row=4, column=3, sticky=W, padx=5, pady=5)
+        Checkbutton(self.root, text="Блок вентилятора ВОСК 2", justify=LEFT, font=("", 9, "bold"), variable=self.vent2box).grid(row=5,
                                                                                                           column=0, sticky=W)  # строка чекбокса
-        self.air2.grid(row=5, column=1, sticky=W)  # выпадающая менюшка аиров
+        self.vosk2.grid(row=5, column=1, sticky=W, padx=5, pady=5)
+        self.vosk2.current(0)
+        self.air2.grid(row=5, column=2, sticky=W, padx=5, pady=5)  # выпадающая менюшка аиров
         self.air2.current(0)  # устанавливаем дефолтную позицию выпадающего меню
-        self.entryvent2.grid(row=5, column=2, sticky=W)
-        Checkbutton(self.root, text="Блок ВНВ 5012", justify=LEFT, variable=self.vnv5012box).grid(row=6, column=0,
+        self.entryvent2.grid(row=5, column=3, sticky=W, padx=5, pady=5)
+        Checkbutton(self.root, text="Блок ВНВ 5012", justify=LEFT, font=("", 9, "bold"), variable=self.vnv5012box).grid(row=6, column=0,
                                                                                                   sticky=W)        # строка чекбокса
-        self.vnv5012_widht.grid(row=6, column=1, sticky=W)  # выпадающая менюшка внв
+        self.vnv5012_widht.grid(row=6, column=1, sticky=W, padx=5, pady=5)  # выпадающая менюшка внв
         self.vnv5012_widht.current(0)  # устанавливаем дефолтную позицию выпадающего меню
-        self.entryvnv5012.grid(row=6, column=2, sticky=W)
+        self.entryvnv5012.grid(row=6, column=2, columnspan=3, sticky=W+E, padx=5, pady=5)
         # Checkbutton(self.root, text="Блок ВНВ 4816", justify=LEFT, variable=self.vnv4816box).grid(row=6, column=0, sticky=W)      # строка чекбокса
         # self.vnv4816_widht.grid(row=6, column=1, sticky=W)  # выпадающая менюшка внв
         # self.vnv4816_widht.current(0)  # устанавливаем дефолтную позицию выпадающего меню
         # self.entryvnv4816.grid(row=6, column=2, columnspan=2, sticky=W)
-        Checkbutton(self.root, text="Блок ВОВ 5012", justify=LEFT, variable=self.vov5012box).grid(row=7, column=0,
+        Checkbutton(self.root, text="Блок ВОВ 5012", justify=LEFT, font=("", 9, "bold"), variable=self.vov5012box).grid(row=7, column=0,
                                                                                                   sticky=W)     # строка чекбокса
-        self.vov5012_widht.grid(row=7, column=1, sticky=W)  # выпадающая менюшка внв
+        self.vov5012_widht.grid(row=7, column=1, sticky=W, padx=5, pady=5)  # выпадающая менюшка внв
         self.vov5012_widht.current(0)  # устанавливаем дефолтную позицию выпадающего меню
-        self.entryvov5012.grid(row=7, column=2, columnspan=2, sticky=W)
+        self.entryvov5012.grid(row=7, column=2, columnspan=3, sticky=W+E, padx=5, pady=5)
         # Checkbutton(self.root, text="Блок ВОВ 4816", justify=LEFT, variable=self.vov4816box).grid(row=8, column=0, sticky=W)       # строка чекбокса
         # self.vov4816_widht.grid(row=8, column=1, sticky=W)  # выпадающая менюшка внв
         # self.vov4816_widht.current(0)  # устанавливаем дефолтную позицию выпадающего меню
         # self.entryvov4816.grid(row=8, column=2, columnspan=2, sticky=W)
-        Checkbutton(self.root, text="Блок ЭКО", justify=LEFT, variable=self.ekobox).grid(row=9, column=0, sticky=W)  # строка чекбокса
-        self.entryeko.grid(row=9, column=1, columnspan=2, sticky=W)
-        Checkbutton(self.root, text="Блок вертикального клапана", justify=LEFT, variable=self.vertklapbox).grid(
+        Checkbutton(self.root, text="Блок ЭКО", justify=LEFT, font=("", 9, "bold"), variable=self.ekobox).grid(row=9, column=0,
+                                                                                          sticky=W)  # строка чекбокса
+        self.entryeko.grid(row=9, column=1, columnspan=3, sticky=W+E, padx=5, pady=5)
+        Checkbutton(self.root, text="Блок вертикального клапана", justify=LEFT, font=("", 9, "bold"), variable=self.vertklapbox).grid(
             row=10, column=0, sticky=W)  # строка чекбокса
-        self.entryvertklap.grid(row=10, column=1, columnspan=2, sticky=W)
-        Checkbutton(self.root, text="Блок пластинчатого утилизатора", justify=LEFT, variable=self.ppbox).grid(
+        self.entryvertklap.grid(row=10, column=1, columnspan=3, sticky=W+E, padx=5, pady=5)
+        Checkbutton(self.root, text="Блок пластинчатого утилизатора", justify=LEFT, font=("", 9, "bold"), variable=self.ppbox).grid(
             row=11, column=0, sticky=W)  # строка чекбокса
-        self.entrypp.grid(row=11, column=1, columnspan=2, sticky=W)
-        Checkbutton(self.root, text="Блок камеры промежуточной",justify=LEFT,variable=self.promkambox).grid(row=12,
+        self.entrypp.grid(row=11, column=1, columnspan=3, sticky=W+E, padx=5, pady=5)
+        Checkbutton(self.root, text="Блок камеры промежуточной",justify=LEFT, font=("", 9, "bold"),
+                    variable=self.promkambox).grid(row=12,
                                                                                                             column=0, sticky=W)  # строка чекбокса
-        self.entrypromkam.grid(row=12, column=1, columnspan=2, sticky=W) # рисуем спинбокс
+        self.entrypromkam.grid(row=12, column=1, columnspan=3, sticky=W+E, padx=5, pady=5) # рисуем спинбокс
 
         Button(self.root, width=6, height=2, text="Выгрузить", font=("Ghost type A", 9, "bold"),
                command=self.action).grid(row=13, column=0, sticky=W+E)                        # Кнопка выгрузки
         Button(self.root, width=6, height=2, text="Закрыть", command=self.root.destroy).grid(row=13, column=1,
-                                                                                             columnspan=2, sticky=W+E)                           # Кнопка закрытия
-        Label(self.root, text="v0.0.4 alfa", justify=LEFT).grid(column=2, sticky=E)
-
-   # def exit(self):
-   #     choice = messagebox.askyesno("Quit", "Do you want to quit?")
-   #     if choice:
-   #         self.root.destroy()
+                                                                                             columnspan=3,
+                                                                                             sticky=W+E)  # Кнопка закрытия
+        Label(self.root, text="v0.0.5 alfa", justify=LEFT).grid(row=14, column=3, sticky=E)
 
 
+    #Конопка INFO
+    def info(self, title="INFO", resizable=(False, False), icon=r"resourses/info.ico"):
+        draw = Tk()
+        draw.title(title)
+        draw.resizable(resizable[0], resizable[1])
+        if icon:
+            draw.iconbitmap(icon)
+
+        Label(draw, text="- Для блоков ВОСК выбираем типоразмер ВОСК, типоразмер двигателя и "
+                                             "количество.\n- Для блока ВНВ и ВОВ "
+                                             "выбираем ширину ТО и количество.\n- Для всего остального на выбор только "
+                                             "количество.", justify=LEFT, padx=15, pady=15, font=("", 10, "bold")).grid(
+                                                                                              row=0, column=0, sticky=W)
+        Button(draw, width=10, height=2, text="Ок", command=draw.destroy).grid(row=1, column=0)
+    #вывод данных в excel
     def action(self):
         global sheet_form
         vrs_num = self.vrs_num.get()
         vrs_perf = self.vrs_perf.get()
+        vosk = self.vosk.get()
+        vosk2 = self.vosk2.get()
+        air = self.air.get()
+        air2 = self.air2.get()
+        vnv = self.vnv5012_widht.get()
+        vov = self.vov5012_widht.get()
         #форматирование заголовка в выводимой таблице
         ws.cell(row=1, column=1).value = 'Перечень VRS-500-' + vrs_num + '-' + vrs_perf
         ws.cell(row=2, column=1).value = 'Обозначение'
@@ -133,9 +166,9 @@ class VRS:
         ws.cell(row=2, column=4).value = 'Материал'
         ws.merge_cells('A1:D1')
         ws.column_dimensions['A'].width = 25
-        ws.column_dimensions['B'].width = 16
+        ws.column_dimensions['B'].width = 35
         ws.column_dimensions['C'].width = 7
-        ws.column_dimensions['D'].width = 11
+        ws.column_dimensions['D'].width = 12
         if self.filterbox.get():
             filteramount = int(self.entryfilter.get())
             ws.append(['Блок фильтра', '', filteramount])
@@ -147,14 +180,31 @@ class VRS:
 
         if self.ventbox.get():
             ventamount = int(self.entryvent.get())
-            ws.append(['Блок вентилятора ' + self.air.get(), '', ventamount])
+            ws.append(['Блок вентилятора ВОСК ' + self.vosk.get() + ' ' + self.air.get(), '', ventamount])
             #ws.append(['Разблюдовка:'])
 
-            cells = vent_table(vrs_num, vrs_perf)
-            for sign, name, amount, material in cells:
+            vent = vent_table(vrs_num, vrs_perf, vosk)
+            air = air_table(vrs_perf, air)
+            for sign, name, amount, material in vent:
+                #print(sign.value, name.value, amount.value, material.value) #проверяем в консоли на вывод нужных строк
+                ws.append([sign.value, name.value, int(amount.value * ventamount), material.value])
+            for sign, name, amount, material in air:
                 #print(sign.value, name.value, amount.value, material.value) #проверяем в консоли на вывод нужных строк
                 ws.append([sign.value, name.value, int(amount.value * ventamount), material.value])
 
+        if self.vent2box.get():
+            ventamount = int(self.entryvent2.get())
+            ws.append(['Блок вентилятора ВОСК ' + self.vosk2.get() + ' ' + self.air2.get(), '', ventamount])
+            #ws.append(['Разблюдовка:'])
+
+            vent2 = vent2_table(vrs_num, vrs_perf, vosk2)
+            air2 = air2_table(vrs_perf, air2)
+            for sign, name, amount, material in vent2:
+                #print(sign.value, name.value, amount.value, material.value) #проверяем в консоли на вывод нужных строк
+                ws.append([sign.value, name.value, int(amount.value * ventamount), material.value])
+            for sign, name, amount, material in air2:
+                #print(sign.value, name.value, amount.value, material.value) #проверяем в консоли на вывод нужных строк
+                ws.append([sign.value, name.value, int(amount.value * ventamount), material.value])
 
 
         if self.filterbox.get() == 0 and self.ventbox.get() == 0 and self.vent2box.get() == 0 and \
--- table_var.py
+++ table_var.py
@@ -44,49 +44,105 @@ def fb_table(vrs_num, vrs_perf):
     for key in filter_table.keys():
         if key == vrs_num:
             cells = filter_table[key]
+            break
     return cells
 
 
-def vent_table(vrs_num, vrs_perf):
+def vent_table(vrs_num, vrs_perf, vosk):
     if vrs_perf == '01':
-        sheet_form = wb_form['Filter-01']  # переходим на вкладку 01 исполнения
+        sheet_form = wb_form['Vent-01']  # переходим на вкладку 01 исполнения
     elif vrs_perf == '02':
-        sheet_form = wb_form['Filter-02']  # переходим на вкладку 02 исполнения
+        sheet_form = wb_form['Vent-02']  # переходим на вкладку 02 исполнения
     elif vrs_perf == '03' or vrs_perf == '04':
-        sheet_form = wb_form['Filter-03(04)']  # переходим на вкладку 03 исполнения
+        sheet_form = wb_form['Vent-03(04)']  # переходим на вкладку 03 исполнения
 
     vent_table = {
-        '019': sheet_form['A5':'D13'],
-        '034': sheet_form['A18':'D26'],
-        '039': sheet_form['A31':'D39'],
-        '054': sheet_form['A44':'D53'],
-        '058': sheet_form['A58':'D67'],
-        '078': sheet_form['A72':'D81'],
-        '086': sheet_form['A86':'D97'],
-        '097': sheet_form['A102':'D111'],
-        '115': sheet_form['A116':'D127'],
-        '116': sheet_form['A132':'D140'],
-        '138': sheet_form['A145':'D154'],
-        '156': sheet_form['A159':'D168'],
-        '173': sheet_form['A173':'D184'],
-        '193': sheet_form['A189':'D198'],
-        '194': sheet_form['A203':'D214'],
-        '215': sheet_form['A219':'D228'],
-        '234': sheet_form['A233':'D242'],
-        '240': sheet_form['A247':'D258'],
-        '271': sheet_form['A263':'D272'],
-        '289': sheet_form['A277':'D288'],
-        '290': sheet_form['A293':'D304'],
-        '333': sheet_form['A309':'D318'],
-        '337': sheet_form['A323':'D334'],
-        '350': sheet_form['A339':'D350'],
-        '407': sheet_form['A355':'D366'],
-        '414': sheet_form['A371':'D382'],
-        '473': sheet_form['A387':'D398'],
-        '500': sheet_form['A403':'D414']
+        '019' + '2.5': sheet_form['A13':'D15'],
+        '019' + '2.8': sheet_form['F13':'I15'],
+        '019' + '3.15': sheet_form['K13':'N15']
     }
 
     for key in vent_table.keys():
-        if key == vrs_num:
-            cells = vent_table[key]
-    return cells
+        if key == vrs_num + vosk:
+            vent = vent_table[key]
+            break
+    return vent
+
+def vent2_table(vrs_num, vrs_perf, vosk):
+    if vrs_perf == '01':
+        sheet_form = wb_form['Vent-01']  # переходим на вкладку 01 исполнения
+    elif vrs_perf == '02':
+        sheet_form = wb_form['Vent-02']  # переходим на вкладку 02 исполнения
+    elif vrs_perf == '03' or vrs_perf == '04':
+        sheet_form = wb_form['Vent-03(04)']  # переходим на вкладку 03 исполнения
+
+    vent_table = {
+        '019' + '2.5': sheet_form['A13':'D15'],
+        '019' + '2.8': sheet_form['F13':'I15'],
+        '019' + '3.15': sheet_form['K13':'N15']
+    }
+
+    for key in vent_table.keys():
+        if key == vrs_num + vosk:
+            vent2 = vent_table[key]
+            break
+    return vent2
+
+def air_table(vrs_perf, air):
+    if vrs_perf == '01':
+        sheet_form = wb_form['Vent-01']  # переходим на вкладку 01 исполнения
+    elif vrs_perf == '02':
+        sheet_form = wb_form['Vent-02']  # переходим на вкладку 02 исполнения
+    elif vrs_perf == '03' or vrs_perf == '04':
+        sheet_form = wb_form['Vent-03(04)']  # переходим на вкладку 03 исполнения
+
+    air_table = {
+        'АИР56': sheet_form['A4':'D9'],
+        'АИР63': sheet_form['F4':'I9'],
+        'АИР71': sheet_form['K4':'N9'],
+        'АИР80': sheet_form['P4':'S9']
+    }
+
+    for key in air_table.keys():
+        if key == air:
+            air = air_table[key]
+            break
+    return air
+
+def air2_table(vrs_perf, air2):
+    if vrs_perf == '01':
+        sheet_form = wb_form['Vent-01']  # переходим на вкладку 01 исполнения
+    elif vrs_perf == '02':
+        sheet_form = wb_form['Vent-02']  # переходим на вкладку 02 исполнения
+    elif vrs_perf == '03' or vrs_perf == '04':
+        sheet_form = wb_form['Vent-03(04)']  # переходим на вкладку 03 исполнения
+
+    air_table = {
+        'АИР56': sheet_form['A4':'D9'],
+        'АИР63': sheet_form['F4':'I9'],
+        'АИР71': sheet_form['K4':'N9'],
+        'АИР80': sheet_form['P4':'S9']
+    }
+
+    for key in air_table.keys():
+        if key == air2:
+            air2 = air_table[key]
+            break
+    return air2
+def vnv5012_table():
+    pass
+
+def v0v5012_table():
+    pass
+
+def eko_table():
+    pass
+
+def vertklap_table():
+    pass
+
+def pp_table():
+    pass
+
+def promkam_table():
+    pass
